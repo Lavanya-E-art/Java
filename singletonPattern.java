fa051f7c150c1609603f743bf41699f6b62af9c1
Lavanya E <63230634+Lavanya-E-art@users.noreply.github.com>
2025-03-12 11:19:16 +0530

    Java program to implement singleton pattern

---
+++ singletonPattern.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    public String str;
+    private static Singleton obj;
+    private Singleton(){}
+    public static Singleton getSingleInstance(){
+        if(obj == null){
+            obj = new Singleton();
+        }
+        return obj;
+    }
+}
+
+
+/*
+Input:
+hello world
+
+Output:
+Hello I am a singleton! Let me say hello world to you
+
+*/
